2b8bd7e32c279202b3f2b706f87114cec6ed1279
Frederic Marion-Poll <marion_poll@hotmail.com>
2025-05-21 12:10:54 +0200

    correct bug / table

--- src/main/java/plugins/fmp/multiSPOTS96/experiment/cages/CageTable.java
+++ src/main/java/plugins/fmp/multiSPOTS96/experiment/cages/CageTable.java
@@ -41,7 +41,7 @@ public class CageTable extends JTable {
 		columnModel.getColumn(3).setPreferredWidth(15);
 		columnModel.getColumn(4).setPreferredWidth(25);
 		columnModel.getColumn(5).setPreferredWidth(15);
-		columnModel.getColumn(7).setPreferredWidth(15);
+		columnModel.getColumn(6).setPreferredWidth(15);
 	}
 
 }
